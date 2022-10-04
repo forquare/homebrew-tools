5106ea702c6b73667ba00000337390c3ad9b1866
forquare <ben@lavery-griffiths.com>
2022-10-04 08:54:52 +0100

    Brew formula update for zht version 0.1.1

--- zht.rb
+++ zht.rb
@@ -5,12 +5,12 @@
 class Zht < Formula
   desc " The ZSH History Tool "
   homepage "https://github.com/forquare/zht"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
-    url "https://github.com/forquare/zht/releases/download/0.1.0/zht_0.1.0_macOS_all.tar.gz"
-    sha256 "cb19b57448a737c9de33ea8814979657428a83bd66af5cab237c6d5c7a7729bf"
+    url "https://github.com/forquare/zht/releases/download/0.1.1/zht_0.1.1_macOS_all.tar.gz"
+    sha256 "9f9d4cea9886cd7925822d45ad97a0ec31674105ccc9d2c03598027d23759143"
 
     def install
       bin.install "zht"
@@ -19,24 +19,24 @@ class Zht < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/forquare/zht/releases/download/0.1.0/zht_0.1.0_Linux_64bit.tar.gz"
-      sha256 "d4382cf11cf1f0ec22266aad3033c7250c69e3c63e7d507427c24551bb272d59"
+      url "https://github.com/forquare/zht/releases/download/0.1.1/zht_0.1.1_Linux_64bit.tar.gz"
+      sha256 "9b1b5e79a2ae95fe6c90cddd7765c353d532497c47f0cb8ec08bd8368bf381b1"
 
       def install
         bin.install "zht"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/forquare/zht/releases/download/0.1.0/zht_0.1.0_Linux_ARM64.tar.gz"
-      sha256 "d2a4c6e51bab79d38d2d1f8272176162221436f27490ce829b5ab89231d8b5b0"
+      url "https://github.com/forquare/zht/releases/download/0.1.1/zht_0.1.1_Linux_ARM64.tar.gz"
+      sha256 "00fb2f5ccd75cdb0d5b2b073c713469fabbc321de162ebc6254b7d9fa3d4d858"
 
       def install
         bin.install "zht"
       end
     end
     if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      url "https://github.com/forquare/zht/releases/download/0.1.0/zht_0.1.0_Linux_ARMv6.tar.gz"
-      sha256 "b2360838f3b196e68a0e0516f4e7a4942d2dc87aa3a16bc601a718acbb19ffa2"
+      url "https://github.com/forquare/zht/releases/download/0.1.1/zht_0.1.1_Linux_ARMv6.tar.gz"
+      sha256 "9298dcaa5e90dcdbf3c9f3d683e8de6d8dc7291d45569cafb5118a13662be4f6"
 
       def install
         bin.install "zht"
