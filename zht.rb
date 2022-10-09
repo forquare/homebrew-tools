d79a1421eac11ac8a3f5bff14faa40639ffc5d06
forquare <ben@lavery-griffiths.com>
2022-10-09 22:00:17 +0100

    Brew formula update for zht version 0.2.0

--- zht.rb
+++ zht.rb
@@ -10,7 +10,7 @@ class Zht < Formula
 
   on_macos do
     url "https://github.com/forquare/zht/releases/download/0.2.0/zht_0.2.0_macOS_all.tar.gz"
-    sha256 "a01677d848a32274378439280ae884142ea740762a89a7f4a0e0a537e639a673"
+    sha256 "0f85d02d9758e9594731322d79e9718dbb4bd5c0e1f6af2e1d99ff10a2900626"
 
     def install
       bin.install "zht"
@@ -18,17 +18,17 @@ class Zht < Formula
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/forquare/zht/releases/download/0.2.0/zht_0.2.0_Linux_64bit.tar.gz"
-      sha256 "d00f3fa3a4c5f2fb3ea1ef26af8a0fd41452ee746bcfba8028d9b533a7c3d6f7"
+    if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
+      url "https://github.com/forquare/zht/releases/download/0.2.0/zht_0.2.0_Linux_ARMv6.tar.gz"
+      sha256 "9162bd5bdd1382dddeedc239a0f803911b2b04d4b248f228d0cc9e2eccc4b893"
 
       def install
         bin.install "zht"
       end
     end
-    if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      url "https://github.com/forquare/zht/releases/download/0.2.0/zht_0.2.0_Linux_ARMv6.tar.gz"
-      sha256 "f3e2c7a96363c46ce2eff537aab1ce4b22d5c8563c456b8c58d4d9b57c76b02a"
+    if Hardware::CPU.intel?
+      url "https://github.com/forquare/zht/releases/download/0.2.0/zht_0.2.0_Linux_64bit.tar.gz"
+      sha256 "1734dc6b9aaebd5c095ba2c27a3f628dca4a1fa249419c34bfea8f67a99f7f11"
 
       def install
         bin.install "zht"
@@ -36,7 +36,7 @@ class Zht < Formula
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/forquare/zht/releases/download/0.2.0/zht_0.2.0_Linux_ARM64.tar.gz"
-      sha256 "3b1617bb6eed1dbe38f5b37fefb667d214ff0087e1f29f17a2b162b0640311e0"
+      sha256 "9fc1e71118cc80e9088bc44bcda1bd66077afc751d38efc1e36b8494faf67d64"
 
       def install
         bin.install "zht"
